3cc4cecf76669e80432d7f9a9ff9436035e0798e
hujinbin <437044048@qq.com>
2022-01-31 23:02:51 +0800

    feat: uni 骨架屏页面

---
+++ uni-skeleton.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="uni-skeleton">
+		<h3 class="uni-skeleton-title" style="width: 38%;"></h3>
+		<ul class="uni-skeleton-paragraph">
+			<li></li>
+			<li></li>
+			<li style="width: 61%;"></li>
+		</ul>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"uni-skeleton",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+.uni-skeleton{
+	width: 100%;
+}
+.uni-skeleton .uni-skeleton-title{
+	height: 16px;
+	margin-top: 16px;
+	background: #f2f2f2;
+}
+.uni-skeleton .uni-skeleton-paragraph{
+	margin: 0;
+	padding: 6px 0 0 0;
+}
+.uni-skeleton .uni-skeleton-paragraph>li{
+	width: 100%;
+	margin: 16px 0 0 0;
+	height: 16px;
+	list-style: none;
+	background: #f2f2f2;
+}
+</style>
